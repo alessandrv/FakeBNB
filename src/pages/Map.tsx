4ea8a41cdcb15cd5656fe9ccf27357b0b4a7fa3b
Alessandro Piccin <alessandropiccin00@gmail.com>
2025-04-02 09:14:56 +0200

    Added white border

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -792,8 +792,8 @@ const SearchedLocationMarker = ({ position }: { position: [number, number] }) =>
       <Marker 
         position={position}
         icon={L.divIcon({
-          html: '<div class="flex justify-center items-center w-full h-full"><div class="w-4 h-4 bg-primary rounded-full shadow-lg"></div></div>',
-          className: 'custom-marker-icon bg-gradient-to-tr from-gradient-first to-gradient-second text-primary-foreground',
+          html: '<div class="flex justify-center items-center w-full h-full"><div class="w-4 h-4 bg-gradient-to-tr from-gradient-first to-gradient-second text-primary-foreground rounded-full shadow-lg"></div></div>',
+          className: 'custom-marker-icon ',
           iconSize: [24, 24],
           iconAnchor: [12, 12]
         })}
@@ -947,7 +947,7 @@ const PersistentMarkers = ({ houses, onMarkerClick, selectedHouseId, findOnMapTi
       {houses.map(house => (
         <Marker 
           icon={L.divIcon({
-            html: '<div class="flex justify-center items-center w-full h-full"><div class="w-6 h-6  bg-gradient-to-tr from-gradient-first to-gradient-second  rounded-full shadow-lg flex items-center justify-center"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4"><path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" /><path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75v4.5a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198c.03-.028.061-.056.091-.086L12 5.432z" /></svg></div></div>',
+            html: '<div class="flex justify-center items-center w-full h-full"><div class="w-6 h-6 bg-gradient-to-tr from-gradient-first to-gradient-second rounded-full shadow-lg flex items-center justify-center border-2 border-white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4"><path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" /><path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75v4.5a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198c.03-.028.061-.056.091-.086L12 5.432z" /></svg></div></div>',
             className: 'custom-marker-icon ',
             iconSize: [28, 28],
             iconAnchor: [14, 14]
